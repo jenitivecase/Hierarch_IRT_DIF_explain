374071a60e363a49b97d7caa1dda603a24a17d33
jenitivecase <jbrussow@gmail.com>
2017-03-08 22:00:54 -0600

    finished combination script (I hope)

--- file_combination.R
+++ file_combination.R
@@ -19,6 +19,7 @@ library(ggplot2)
 files <- as.data.frame(list.files(getwd()))
 names(files) <- "filename"
 files$filename <- as.character(files$filename)
+files <- filter(files, filename != "combined")
 
 for(i in 1:nrow(files)){
   info <-unlist(strsplit(as.character(
@@ -39,9 +40,25 @@ types_conditions <- expand.grid(conditions, types)
 names(types_conditions) <- c("conditions", "types")
 types_conditions <- apply(types_conditions, 2, as.character)
 
+if(!dir.exists("combined")){dir.create("combined")}
+
 for(i in 1:nrow(types_conditions)){
   condition_matches <- grep(types_conditions[i, "conditions"], files$filename)
   type_matches <- grep(types_conditions[i, "types"], files$filename)
   indices <- condition_matches[which(condition_matches %in% type_matches)]
   set_files <- files[indices, "filename"]
+  
+  out <- NA
+  
+  for(j in 1:length(set_files)){
+    output <- readRDS(set_files[j])
+    output <- output[!sapply(output, is.null)] 
+    out <- append(out, output)
+  }
+  
+  out <- out[c(2:length(out))]
+  
+  fname <- paste0("combined_", types_conditions[i, "types"], "_", types_conditions[i, "conditions"], ".rds")
+  saveRDS(out, paste0("combined/", fname))
+  
 }
