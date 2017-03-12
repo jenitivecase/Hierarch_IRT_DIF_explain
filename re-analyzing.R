43ac7679118bd880f1d305217c0321ac3faead6b
jenitivecase <jbrussow@gmail.com>
2017-03-12 13:08:22 -0500

    re-analysis script complete. here's hoping for RAM luck

--- re-analyzing.R
+++ re-analyzing.R
@@ -5,7 +5,7 @@ options(scipen = 999)
 date <- format.Date(Sys.Date(), "%Y%m%d")
 
 # work_dir <- "C:/Users/jbrussow/Dropbox/REMS/11 Comps/Simulation/20170224_simulation-results"
-work_dir <- "F:/Comps simulation results 20170302/All_simulation_results_20170224-0306"
+work_dir <- "D:/Comps simulation results 20170302/All_simulation_results_20170224-0306"
 
 if(Sys.info()["user"] == "jbrussow"){
   setwd(work_dir)
@@ -17,12 +17,13 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(rstan)
+library(robustbase)
 
 files <- as.data.frame(list.files(getwd()))
 names(files) <- "filename"
 files$filename <- as.character(files$filename)
 files <- filter(files, filename != "combined")
-
+  
 for(i in 1:nrow(files)){
   info <-unlist(strsplit(as.character(
     files[i, "filename"]), "_"))
@@ -33,6 +34,9 @@ for(i in 1:nrow(files)){
   type <- gsub("\\_$", "", type)
   files[i, "condition"] <- condition
   files[i, "type"] <- type
+  file_tag <- paste0(c(info[(len-2):len]), collapse = "", sep = "_")
+  file_tag <- gsub("\\.rds\\_", "", file_tag)
+  files[i, "file_tag"] <- file_tag
 }
 
 conditions <- unique(files$condition)
@@ -44,15 +48,50 @@ types_conditions <- apply(types_conditions, 2, as.character)
 
 results_files <- files[which(grepl("result", files$filename)),]
 
+
 for(i in 1:nrow(results_files)){
   output <- readRDS(results_files[i, "filename"])
   output <- output[!sapply(output, is.null)] 
+  
+  est_param_summary <- vector("list", length(output))
+  est_param_medians <- vector("list", length(output))
+  median_correlations <- vector("list", length(output))
   
   for(j in 1:length(output)){
     params_summary <- summary(output[[j]], pars = c("a", "b", "D", "beta1", "mu", 
                                                  "sigma2", "R2", "theta",
                                                  "foc_mean"),
-                              probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary  
+                              probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary 
+    
+    est_param_summary[[j]] <- params_summary
+    
+    params <- extract(output[[j]], pars = c("a", "b", "D", "beta1", "mu", "sigma2", 
+                                         "R2", "theta", "foc_mean"))
+    
+    a_params <- as.matrix(colMedians(params$a))
+    b_params <- as.matrix(colMedians(params$b))
+    D_params <- as.matrix(colMedians(params$D))
+    beta1 <- median(params$beta1)
+    mu <- as.matrix(colMedians(params$mu))
+    sigma2 <- median(params$sigma2)
+    R2 <- median(params$R2)
+    theta <- as.matrix(colMedians(params$theta))
+    foc_mean <- median(params$foc_mean)
+    
+    #save the medians of estimated parameters
+    est_param_medians[[j]] <- list(a_params, b_params, D_params, beta1, 
+                                 mu, sigma2, R2, theta, foc_mean)
+    names(est_param_medians[[j]]) <- c("a_params", "b_params", "D_params", 
+                                     "beta1", "mu", "sigma2", "R2", "theta", 
+                                     "foc_mean")
+    
   }
   
+  saveRDS(est_param_summary, paste0("newest_param_summary_", 
+                                    results_files[i, "file_tag"], ".rds"))
+  saveRDS(est_param_medians, paste0("est_param_medians_", 
+                                    results_files[i, "file_tag"], ".rds"))
+  
+  rm(output)
+  gc()
 }
