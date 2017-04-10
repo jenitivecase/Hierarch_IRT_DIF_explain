8de8b70c3c8f90cf21584ba91949525006506531
jenitivecase <jbrussow@gmail.com>
2017-04-10 12:05:50 -0500

    updated to pull in true params correctly

--- re-analyzing.R
+++ re-analyzing.R
@@ -5,7 +5,7 @@ options(scipen = 999)
 date <- format.Date(Sys.Date(), "%Y%m%d")
 
 # work_dir <- "C:/Users/jbrussow/Dropbox/REMS/11 Comps/Simulation/20170224_simulation-results"
-work_dir <- "D:/Comps simulation results 20170302/All_simulation_results_20170224-0306"
+work_dir <- "S:/Projects/DLM Secure/Psychometrician Asst Projects/Jennifer Projects/TEST/All_simulation_results_20170224-0306"
 
 if(Sys.info()["user"] == "jbrussow"){
   setwd(work_dir)
@@ -53,7 +53,13 @@ for(i in 1:nrow(results_files)){
   output <- readRDS(results_files[i, "filename"])
   output <- output[!sapply(output, is.null)] 
   
-  # params_extraction <- vector("list", length(output))
+  n_ref <- gsub("PREF", "", unlist(strsplit(results_files[1,"condition"], "_"))[2])
+  n_ref <- as.numeric(gsub("-", "\\.", n_ref))*1000
+  
+  rho <- gsub("rho", "", unlist(strsplit(results_files[1,"condition"], "_"))[1])
+  rho <- as.numeric(gsub("-", "\\.", rho))
+  
+  params_extraction <- vector("list", length(output))
   # est_param_summary <- vector("list", length(output))
   # est_param_medians <- vector("list", length(output))
   median_correlations <- vector("list", length(output))
@@ -61,17 +67,17 @@ for(i in 1:nrow(results_files)){
   true_params <- readRDS(true_param_files[which(true_param_files$file_tag == results_files[i, "file_tag"]),"filename"])
     
   for(j in 1:length(output)){
-    # params_summary <- summary(output[[j]], pars = c("a", "b", "D", "beta1", "mu", 
-    #                                              "sigma2", "R2", "theta",
-    #                                              "foc_mean"),
-    #                           probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary 
-    # 
-    # est_param_summary[[j]] <- params_summary
+    params_summary <- summary(output[[j]], pars = c("a", "b", "D", "beta1", "mu",
+                                                 "sigma2", "R2", "theta",
+                                                 "foc_mean"),
+                              probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary
+
+    est_param_summary[[j]] <- params_summary
 
     params <- extract(output[[j]], pars = c("a", "b", "D", "beta1", "mu", "sigma2",
                                          "R2", "theta", "foc_mean"))
     
-    # params_extraction[[i]] <- params
+    params_extraction[[j]] <- params
 
     a_params <- as.matrix(colMedians(params$a))
     b_params <- as.matrix(colMedians(params$b))
@@ -91,12 +97,12 @@ for(i in 1:nrow(results_files)){
     #                                  "foc_mean")
     
     #save the median correlations & differences from the expected values
-    a_corr <- cor(a_params, true_item_params[,"a_param"])
-    b_corr <- cor(b_params, true_item_params[,"b_param"])
-    D_corr <- cor(D_params, true_item_params[,"dif_param"])
-    theta_corr <- cor(theta, true_ability[, 1])
+    a_corr <- cor(a_params, true_params[[j]][["true_item_params"]][,"a_param"])
+    b_corr <- cor(b_params, true_params[[j]][["true_item_params"]][,"b_param"])
+    D_corr <- cor(D_params, true_params[[j]][["true_item_params"]][,"dif_param"])
+    theta_corr <- cor(theta, true_params[[j]][["true_ability"]][,"theta"])
     foc_mean_diff <- -.5-foc_mean
-    ref_mean_diff <- 0-mean(theta[1:n_ref])
+    ref_mean_diff <- 0-mean(true_params[[j]][["true_ability"]][1:n_ref,"theta"])
     R2_diff <- (rho^2)-R2
     
     median_correlations[[j]] <- list(a_corr, b_corr, D_corr, theta_corr, 
@@ -110,8 +116,8 @@ for(i in 1:nrow(results_files)){
   #                                   results_files[i, "file_tag"], ".rds"))
   # saveRDS(est_param_medians, paste0("est_param_medians_",
   #                                   results_files[i, "file_tag"], ".rds"))
-  # saveRDS(params_extraction, paste0("params_extraction_",
-  #                                   results_files[i, "file_tag"], ".rds"))
+  saveRDS(params_extraction, paste0("params_extraction_",
+                                    results_files[i, "file_tag"], ".rds"))
   saveRDS(median_correlations, paste0("median_correlations_",
                                     results_files[i, "file_tag"], ".rds"))
   
