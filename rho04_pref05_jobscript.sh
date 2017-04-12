492ee874bd26afe31228bcc0a8536de9456114f7
jenitivecase <jbrussow@gmail.com>
2017-04-12 13:47:24 -0500

    fixed submission script

--- rho04_pref05_jobscript.sh
+++ rho04_pref05_jobscript.sh
@@ -1,12 +1,16 @@
-#!/bin/bash 
-#MSUB -l nodes=1:ppn=3,mem=16gb,walltime=60:00:00:00
-#MSUB -M jbrussow@gmail.com
-#MSUB -m abe 
 #MSUB -N IRT_DIF_rho04_pref05
+#MSUB -l nodes=1:ppn=3,walltime=60:00:00:00
+#MSUB -l pmem=16gb
+#MSUB -M jbrussow@ku.edu
+#MSUB -m abe 
 #MSUB -j oe 
+#MSUB -q crmda
+#MSUB -o rho04_pref05_out.log
+
+cd $PBS_O_WORKDIR
 
 module purge
 module use /panfs/pfs.local/work/crmda/tools/modules
 module load Rstats/3.3
 
-R --vanilla -f $HOME/IRT_DIF/cluster_rho04_pref05.R 
+R -f cluster_rho04_pref_05.R
