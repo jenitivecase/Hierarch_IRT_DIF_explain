0c6f26475b09fd885b7666fa6176b4a42420e726
Jennifer Brussow <jbrussow@gmail.com>
2017-04-14 13:08:54 -0500

    rough decision consistency table & graph

---
+++ decision_consistency.R
@@ -0,0 +1,20 @@
+ggplot(data, aes(x = true_param, y = est_param)) + 
+  geom_point(alpha = 0.65, color = colors[i]) + 
+  ggtitle(paste0("D-parameter correlations for\nrho = ", rho, ", reference proportion = ", PREF)) + 
+  labs(x = "True Parameter Value", y = "Estimated Parameter Value", 
+       caption = paste0("r = ", round(cor(data$true_param, data$est_param), 3))) + 
+  theme(plot.title = element_text(hjust = 0.5))  + 
+  geom_line(y = .5) + 
+  geom_vline(xintercept = .5)
+
+flag_amt <- .5
+
+trueneg <- nrow(data[which(data$true_param < flag_amt & data$est_param < flag_amt),])
+falsepos <- nrow(data[which(data$true_param < flag_amt & data$est_param > flag_amt),])
+falseneg <- nrow(data[which(data$true_param > flag_amt & data$est_param < flag_amt),])
+truepos <- nrow(data[which(data$true_param > flag_amt & data$est_param > flag_amt),])
+
+dec_consist <- matrix(c(truepos, falsepos, falseneg, trueneg), nrow = 2, byrow = TRUE)
+colnames(dec_consist) <- c("True_DIF", "True_NoDIF")
+rownames(dec_consist) <- c("Est_DIF", "Est_NoDIF")
+dec_consist
