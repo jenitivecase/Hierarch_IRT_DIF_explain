e5ca842da27d98135c93bcbb4b4fd5621b1c06ad
Jennifer Brussow <jbrussow@gmail.com>
2017-04-14 13:26:57 -0500

    trying to update graph to show classification accuracy; fighting with ggplot alpha

--- decision_consistency.R
+++ decision_consistency.R
@@ -1,3 +1,4 @@
+#alpha setting not working, very annoying
 ggplot(data, aes(x = true_param, y = est_param)) + 
   geom_point(alpha = 0.65, color = colors[i]) + 
   ggtitle(paste0("D-parameter correlations for\nrho = ", rho, ", reference proportion = ", PREF)) + 
@@ -5,7 +6,9 @@ ggplot(data, aes(x = true_param, y = est_param)) +
        caption = paste0("r = ", round(cor(data$true_param, data$est_param), 3))) + 
   theme(plot.title = element_text(hjust = 0.5))  + 
   geom_line(y = .5) + 
-  geom_vline(xintercept = .5)
+  geom_vline(xintercept = .5)+
+  geom_rect(aes(xmin = flag_amt, xmax = Inf, ymin = -Inf, ymax = flag_amt, 
+            fill = "red", alpha = 0.02), show.legend = FALSE)
 
 flag_amt <- .5
 
