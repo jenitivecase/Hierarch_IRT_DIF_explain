cd6ea545ea6cd4d17faa395cd30a1b2b67037c3e
jenitivecase <jbrussow@gmail.com>
2017-04-24 15:09:43 -0500

    upped to 12K iterations, 5K burnin

--- Cluster_stuff/cluster_base_script.R
+++ Cluster_stuff/cluster_base_script.R
@@ -108,7 +108,7 @@ for(i in 1:nreps){
   
   #conducting the analysis
   analysis <- sampling(precomp_model, data = b.dat_long,
-                       iter = 10000, warmup = 5000, chains = 2, verbose = FALSE, cores = 2)
+                       iter = 12000, warmup = 5000, chains = 2, verbose = FALSE, cores = 2)
   
   #save the analysis object
   # result_objs[[i]] <- analysis
