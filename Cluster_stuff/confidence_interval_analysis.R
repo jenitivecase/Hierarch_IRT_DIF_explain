76d51b61922819c4bf4ef1b0782afa8b924f56b0
jenitivecase <jbrussow@gmail.com>
2017-06-01 16:26:02 -0500

    eliminated unnecessary parameters

--- Cluster_stuff/confidence_interval_analysis.R
+++ Cluster_stuff/confidence_interval_analysis.R
@@ -5,8 +5,6 @@ param_types$dim <- c(rep("vec", 3), rep("scalar", 2),
                      rep("vec", 1), rep("scalar", 2), 
                      rep("vec", 1), rep("scalar", 1))
 
-param_summary_types <- vector("list", length(param_types))
-
 for(i in 1:nrow(param_types)){
   if(param_types[i, "dim"] == "vec"){
     assign(paste0(param_types[i, "param"], "_params_summary"), params_summary[
@@ -17,7 +15,7 @@ for(i in 1:nrow(param_types)){
   }
 }
 
-#item_params: a, b, D, 
+#item_params: a, b, D
 a_param_CIs <- CI_retrieval(true_item_params[, "a_param"], a_params_summary)
 sum(a_param_CIs)/n_items
 
@@ -31,15 +29,10 @@ sum(D_param_CIs)/n_items
 theta_param_CIs <- CI_retrieval(true_ability[, "theta"], theta_params_summary)
 sum(theta_param_CIs)/n_people
 
-#scalar params: beta0, beta1, sigma2, R2, foc_mean
+#scalar params: beta0, beta1, R2 
 beta0_CIs <- CI_retrieval(beta0_true, t(beta0_params_summary))
 
 beta1_CIs <- CI_retrieval(beta1_true, t(beta1_params_summary))
 
-sigma2_CIs <- CI_retrieval(sigma2, t(sigma2_params_summary))
+R2_CIs <- CI_retrieval(R2_true, t(R2_params_summary))
 
-R2_CIs <- CI_retrieval(R2, t(R2_params_summary))
-
-foc_mean_CIs <- CI_retrieval(foc_mean, t(foc_mean_params_summary))
-
-###WHAT TO DO WITH MU?
