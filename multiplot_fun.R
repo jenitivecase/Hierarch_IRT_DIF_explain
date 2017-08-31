8189d80c427497b41b722ea3a1cfccd8d322e987
jenitivecase <jbrussow@gmail.com>
2017-08-31 09:48:56 -0500

    fixed display for non-titled plots

--- multiplot_fun.R
+++ multiplot_fun.R
@@ -1,4 +1,4 @@
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL, title = NULL) {
+multiplot <- function(..., plotlist=NULL, file, cols=1, plot_layout=NULL, plot_title = NULL) {
   library(grid)
   
   # Make a list from the ... arguments and plotlist
@@ -6,17 +6,17 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL, title = NUL
   
   numPlots = length(plots)
   
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
+  # If plot_layout is NULL, then use 'cols' to determine plot_layout
+  if (is.null(plot_layout)) {
     # Make the panel
     # ncol: Number of columns of plots
     # nrow: Number of rows needed, calculated from # of cols
-    # if title argument given, add an extra row here
-    if(is.null(title)){
-      layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+    # if plot_title argument given, add an extra row here
+    if(is.null(plot_title)){
+      plot_layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
                        ncol = cols, nrow = ceiling(numPlots/cols), byrow = TRUE)
     } else {
-      layout <- matrix(c(rep(0, cols), seq(1, cols * (ceiling(numPlots/cols)))),
+      plot_layout <- matrix(c(rep(0, cols), seq(1, cols * (ceiling(numPlots/cols)))),
                        ncol = cols, nrow = (ceiling(numPlots/cols)+1), byrow = TRUE)
     }
   }
@@ -27,26 +27,28 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL, title = NUL
   } else {
     # Set up the page
     grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout), 
-                                               heights = unit(c(1, rep(4, nrow(layout)-1)), "null"))))
-    
+
     # Make each plot, in the correct location
-    if(is.null(title)){
+    if(is.null(plot_title)){
+      pushViewport(viewport(layout = grid.layout(nrow(plot_layout), ncol(plot_layout))))
       
       for(i in 1:numPlots) {
         # Get the i,j matrix positions of the regions that contain this subplot
-        matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+        matchidx <- as.data.frame(which(plot_layout == i, arr.ind = TRUE))
         
         print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
                                         layout.pos.col = matchidx$col))
       }
     } else {
-      grid.text(title, gp = gpar(fontsize = 20, fontface = "bold"),
-                vp = viewport(layout.pos.row = 1, layout.pos.col = c(1:ncol(layout))))
+      pushViewport(viewport(layout = grid.layout(nrow(plot_layout), ncol(plot_layout), 
+                                                 heights = unit(c(1, rep(4, nrow(plot_layout)-1)), "null"))))
+      
+      grid.text(plot_title, gp = gpar(fontsize = 20, fontface = "bold"),
+                vp = viewport(layout.pos.row = 1, layout.pos.col = c(1:ncol(plot_layout))))
       
       for(i in 1:numPlots) {
         # Get the i,j matrix positions of the regions that contain this subplot
-        matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+        matchidx <- as.data.frame(which(plot_layout == i, arr.ind = TRUE))
         
         print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
                                         layout.pos.col = matchidx$col))
