de9714d2de66d2a3d7c06d673179dbdfdd677609
jenitivecase <jbrussow@gmail.com>
2018-04-24 12:10:29 -0500

    model runs

--- real_data_application/real_data_estimation.R
+++ real_data_application/real_data_estimation.R
@@ -2,7 +2,7 @@
 
 work_dir <- paste0(getwd(), "/real_data_application/")
 
-source("mixture_functions.R")
+source(paste0(work_dir, "mixture_functions.R"))
 date <- format.Date(Sys.Date(), "%Y%m%d")
 options(scipen = 999)
 
@@ -18,20 +18,43 @@ n_people <- nrow(responses)
 n_items <- ncol(responses) - 2
 
 items <- read.csv(paste0(work_dir, "items.csv"))
+items <- as.data.frame(apply(items, 2, FUN = as.numeric))
 
 group <- responses$esl
 
 responses <- responses %>%
-  select(-esl) %>%
-  gather(key = "question", value = "response", -fid, na.rm = TRUE)
+  select(-fid) %>%
+  mutate(id = row_number()) %>%
+  gather(key = "question", value = "response", -id, -esl, na.rm = FALSE)
+
+
+n_observations <- nrow(responses)
+respondentid <- responses$id
+itemid <- as.numeric(gsub("scr", "", responses$question))
+response <- responses$response
+group_long <- responses$esl
+
+words <- items$words
+sentences <- items$sentences
+ave_word_sent <- items$ave_word_sent
+bigword <- items$bigword
+nonmath_word <- items$NonmathWord
+prep <- items$prep
+passive <- items$passive
+confuse <- items$confuse
+schematic <- items$schematic
+confuseXschem <- items$confuseXschem
+
 
 #### STAN SETUP ####
 #load stan model scripts
-source("stan_scripts.R")
+source(paste0(work_dir, "real_stan_scripts.R"))
 
 b.dat_long <- list("n_people", "n_items", "n_observations", "respondentid", 
                    "itemid", "response", "group", "group_long",
-                   "DIFpredict")
+                   "words", "sentences", "ave_word_sent", "bigword", 
+                   "nonmath_word", "prep", "passive", "confuse",
+                   "schematic", "confuseXschem")
 
 
 #analysis setup
@@ -40,7 +63,5 @@ precomp_model <- stan_model(stanc_ret = precomp)
 
 #### DATA SAVE SETUP ####
 
-
-
 analysis <- sampling(precomp_model, data = b.dat_long,
-                     iter = 12000, warmup = 5000, chains = 2, verbose = FALSE, cores = 2)
+                     iter = 15000, warmup = 5000, chains = 2, verbose = FALSE, cores = 2)
